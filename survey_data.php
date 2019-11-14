d025a7ff8d82708893b6d446309fabe9d291aeb2
Matthew Warlick Wilson <31708449+mwarlick@users.noreply.github.com>
2019-11-14 16:11:59 -0500

    Added survey_data.php
    created by Brian Drawert

---
+++ survey_data.php
@@ -0,0 +1,44 @@
+<?
+header("Content-Type: application/json; charset=UTF-8");
+
+
+$servername = "138.68.228.126";
+$username = "drawertl_westngn";
+$password = "dbpass_adh4enal";
+$dbname = "drawertl_westngn";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    echo("Connection failed: " . $conn->connect_error);
+    exit(0);
+}
+
+$sql = "SELECT latitude, longitude FROM MLABS_speed_data";
+$result = $conn->query($sql);
+
+if ($conn->error) {
+    echo("Query failed: ".$conn->error);
+    exit(0);
+}
+
+
+echo 'survey_result_points = {
+"type": "FeatureCollection",
+"features": [ 
+';
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo '{ "type": "Feature", "geometry": { "type": "Point", "coordinates": [ '.$row["longitude"].', '.$row['latitude'].' ] } },'."\n";
+    }
+}else{
+    echo("No Data found");
+    exit(0);
+}
+$conn->close();
+
+echo ']
+}
+';
