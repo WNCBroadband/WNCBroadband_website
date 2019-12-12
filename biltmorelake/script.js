68f2e649dc1f2a0e21993c3d49145ead2e365d46
Matthew Warlick Wilson <31708449+mwarlick@users.noreply.github.com>
2019-12-12 15:49:06 -0500

    Update to Script.js
    In the process of changing script.js and adding color coding

--- biltmorelake/script.js
+++ biltmorelake/script.js
@@ -1,7 +1,7 @@
 // Initialize the map and assign it to a variable for later use
 var map = L.map('map', {
     // Set latitude and longitude of the map center (required)
-    center: [35.502,-82.528], 
+    center: [35.55,-82.628], 
     // Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
     zoom: 9,
     maxZoom: 15
@@ -11,10 +11,13 @@ L.control.scale().addTo(map);
 $(".leaflet-control-zoom").css("visibility","hidden");
 // Create a Tile Layer and add it to the map
 
+//Create access token linking to the map on a mappbox account
 survey_results = L.tileLayer('https://api.mapbox.com/styles/v1/westngn/ck2z53u892diw1cldqewni6bg/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2VzdG5nbiIsImEiOiJjanVzc3I1Zm8wMG5mNDNrZnl1aGt5cmhvIn0.0AULTTBaoxcqci1Iie9IDA', {
 }).addTo(map);
 
-survey_points = L.geoJSON(survey_result_points)
+survey_points = L.geoJSON(survey_result_points);
+console.log(survey_points);
+console.log(markers);
 
 var markers = L.markerClusterGroup();
 
@@ -24,11 +27,17 @@ markers.on('clusterclick', function (a) {
     a.layer.zoomToBounds({padding: [20,20]});
 });
 
+for (var i = 0; i < markers.length; i++) {
+		var a = markers[i];
+		var title = a[2];
+		var marker = L.marker(L.latLng(a[0], a[1]), { properties: properties });
+		marker.bindPopup("properties");
+		markers.addLayer(marker);
+}
+
 map.addLayer(markers);
 
-  var searchControl = new L.esri.Controls.Geosearch().addTo(map);
-
-  var results = new L.LayerGroup().addTo(map);
+console.log(markers);
 
   searchControl.on('results', function(data){
     results.clearLayers();
@@ -38,3 +47,6 @@ map.addLayer(markers);
   });
 
 setTimeout(function(){$('.pointer').fadeOut('slow');},3400);
+
+
+
