603dc6bdd6960de9eb7d06da1fdfcb3de9c9737c
cbozarth1 <57452906+cbozarth1@users.noreply.github.com>
2020-03-13 22:29:15 -0400

    Add new survey links to maps

--- upperhominy/map/uhMap.php
+++ upperhominy/map/uhMap.php
@@ -5,32 +5,32 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title>Area Map</title>
    
-  <!--	Leaflet CSS	-->
+  <!--  Leaflet CSS -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"/>
-	
+  
   <!-- Bootstrap core CSS -->
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- JQuery -->
-  <script src="../js/jquery.min.js"></script> 	
+  <script src="../js/jquery.min.js"></script>   
   <!-- Bootstrap core CSS -->
   <link href="../vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
   <!-- Custom fonts for this template -->
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
   <!--Site Stylesheet-->
   <link href="../../css/style.css" rel="stylesheet" type="text/css">
-	
-  <!--	Lealet Javascript	-->
+  
+  <!--  Lealet Javascript -->
   <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"></script>
   <script src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet/0.0.1-beta.5/esri-leaflet.js"></script>
   <script src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.js"></script>
   <script src="leaflet.featuregroup.subgroup.js"></script>
 
-  <!--	Leaflet Stylesheets	-->
+  <!--  Leaflet Stylesheets -->
   <link rel="stylesheet" type="text/css" href="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.css">
   <link href="../vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
   <link href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css" rel="stylesheet">
   <link href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css" rel="stylesheet">
-	
+  
   <!-- Custom fonts for this template -->
   <link href="../vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
@@ -39,11 +39,11 @@
   <link href="../css/landing-page.min.css" rel="stylesheet">
   <script src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></script>
   <link rel="stylesheet" href="../css/mapstyle.css">
-</head>	
+</head> 
 
 <body>
-	  
-	<!-- Navigation -->
+    
+  <!-- Navigation -->
   <nav class="navbar navbar-expand-lg bg-dblue navbar-dark mainnav2">
       <a class="navbar-brand" href="https://wncbroadband.org/blog">
         <img src="../img/wncbroadbandlogo.png" alt="WNC Broadband Project Logo Image and Link" class="img-fluid logo" width="100px">
@@ -95,7 +95,7 @@
             <a class="nav-link" href="../aboutUpperHominy.html">About Community</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="../survey/survey.html">Survey</a>
+            <a class="nav-link" href="../../survey/upperhominy-survey/survey.html">Survey</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="../providers.html">Service Providers</a>
@@ -113,22 +113,22 @@
         </ul>
       </div>
   </nav>
-<!--	END NAVBAR	-->
+<!--  END NAVBAR  -->
 
-	
-<!--	MAP		-->
-  	<div id="map"></div>
+  
+<!--  MAP   -->
+    <div id="map"></div>
 
-<!--	<div id="mapKey">
-		<ul><h3>Map Key</h3>
-			<h5>Download Speed:</h5>
-			<li><img src="img/pin_gray.png" height="20px" width="20px"> 	Gray: Unknown Speed</li>
-			<li><img src="img/pin_red.png" height="20px" width="20px">		Red: Less than 5 Mpbs</li>
-			<li><img src="img/pin_orange.png" height="20px" width="20px">	Orange: Between 5 and 25 Mbps </li>
-			<li><img src="img/pin_yellow.png" height="20px" width="20px">	Yellow: Between 25 and 90 Mbps</li>
-			<li><img src="img/pin_green.png" height="20px" width="20px">	Green: Greater than 90 Mbps</li>
-		</ul>
-	</div>-->
+<!--  <div id="mapKey">
+    <ul><h3>Map Key</h3>
+      <h5>Download Speed:</h5>
+      <li><img src="img/pin_gray.png" height="20px" width="20px">   Gray: Unknown Speed</li>
+      <li><img src="img/pin_red.png" height="20px" width="20px">    Red: Less than 5 Mpbs</li>
+      <li><img src="img/pin_orange.png" height="20px" width="20px"> Orange: Between 5 and 25 Mbps </li>
+      <li><img src="img/pin_yellow.png" height="20px" width="20px"> Yellow: Between 25 and 90 Mbps</li>
+      <li><img src="img/pin_green.png" height="20px" width="20px">  Green: Greater than 90 Mbps</li>
+    </ul>
+  </div>-->
 
   <div class="mapkey">
       <button type="button" class="mapkeycollapsible"><h3>Map Key</h3></button>
@@ -143,9 +143,9 @@
         </ul>
       </div>
       </div>
-        <!--	MAP	END		-->
+        <!--  MAP END   -->
 
-	
+  
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
     <?php
@@ -157,11 +157,11 @@
     ?>
     <script  src="uhMapScript.js"></script>
     <script src="mapkey.js"></script>
-	
-	<!-- Bootstrap core JavaScript -->
+  
+  <!-- Bootstrap core JavaScript -->
   <script src="../vendor/jquery/jquery.slim.min.js"></script>
   <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	  
+    
 
 
 </body>
