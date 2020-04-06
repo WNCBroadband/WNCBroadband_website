b5a2d541f7da79acfd35eba992231a1df91c3275
Matthew Warlick Wilson <31708449+mwarlick@users.noreply.github.com>
2020-04-06 08:51:44 -0400

    Service Map Tests with updated Location Data
    Now has service plan popups for each address!

---
+++ biltmorelake/map/testMap.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Area Map</title>
+   
+  <!--	Leaflet CSS	-->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"/>
+	
+  <!-- Bootstrap core CSS -->
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- JQuery -->
+  <script src="../js/jquery.min.js"></script> 	
+  <!-- Bootstrap core CSS -->
+  <link href="../vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <!--Site Stylesheet-->
+  <link href="../css/style.css" rel="stylesheet" type="text/css">
+	
+  <!--	Lealet Javascript	-->
+  <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"></script>
+  <script src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet/0.0.1-beta.5/esri-leaflet.js"></script>
+  <script src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.js"></script>
+  <script src="leaflet.featuregroup.subgroup.js"></script>
+
+  <!--	Leaflet Stylesheets	-->
+  <link rel="stylesheet" type="text/css" href="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.css">
+  <link href="../vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css" rel="stylesheet">
+  <link href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css" rel="stylesheet">
+	
+  <!-- Custom fonts for this template -->
+  <link href="../vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  
+  <!-- Custom styles for this template -->
+  <link href="../css/landing-page.min.css" rel="stylesheet">
+  <script src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></script>
+  <link rel="stylesheet" href="../css/mapstyle.css">
+</head>	
+
+<body>
+	  
+	<!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dblue fixed-top stroke">
+    <div class="container">
+      <a class="navbar-brand" href="../index.html">
+        <img src="../img/wncbroadbandlogo.png" width="100px" alt="WNC Broadband Project Logo Image and Link">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="../index.html">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="../aboutproject.html">WNC Broadband Project</a></li>
+              <li><a class="dropdown-item" href="../aboutBiltmoreLake.html">Biltmore Lake</a></li>
+           </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../broadband101.html">Broadband 101</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../101links.html">Resources</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../providers.html">Service Providers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../faq.html">FAQ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../contact.html">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Area Maps</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item active" href="../map/map.html">Biltmore Lake</a></li>
+           </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--	END NAVBAR	-->
+
+	
+<!--	MAP		-->
+  	<div id="map"></div>
+  	<!-- 
+	<div id="mapKey">
+		<ul><h3>Map Key</h3>
+			<h5>Download Speed:</h5>
+			<li><img src="img/pin_gray.png" height="20px" width="20px"> 	Gray: Unknown Speed</li>
+			<li><img src="img/pin_red.png" height="20px" width="20px">		Red: Less than 5 Mpbs</li>
+			<li><img src="img/pin_orange.png" height="20px" width="20px">	Orange: Between 5 and 25 Mbps </li>
+			<li><img src="img/pin_yellow.png" height="20px" width="20px">	Yellow: Between 25 and 90 Mbps</li>
+			<li><img src="img/pin_green.png" height="20px" width="20px">	Green: Greater than 90 Mbps</li>
+		</ul>
+	</div>
+	-->
+        <!--	MAP	END		-->
+
+	
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+    <?php
+        if(isset($_GET['survey_id']) && !empty($_GET['survey_id'])){
+    echo '<script src="survey_data.php?survey_id='.$_GET['survey_id'].'"></script>';
+        }else{
+    echo '<script src="survey_data.php?survey_id=1"></script>';
+        }
+    ?>
+    <script  src="testMapScript.js"></script>
+	
+	<!-- Bootstrap core JavaScript -->
+  <script src="../vendor/jquery/jquery.slim.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	  
+
+
+</body>
+</html>
---
+++ biltmorelake/map/testMapScript.js
@@ -0,0 +1,449 @@
+ var servicelatlngs = [
+  {
+    "Address Number": "103 Ginger Quill Cir.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5353017, -82.6518467)"
+  },
+  {
+    "Address Number": "107 Ginger Quill Cir.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5349563, -82.6517623)"
+  },
+  {
+    "Address Number": "155 Gigner Quill Cir.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5361788, -82.6512348)"
+  },
+  {
+    "Address Number": "162 Ginger Quill Cir.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5367459, -82.6507706)"
+  },
+  {
+    "Address Number": "101 Lake Dr.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.533726, -82.6521632)"
+  },
+  {
+    "Address Number": "80 Lake Dr.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5397774, -82.5123035)"
+  },
+  {
+    "Address Number": "128 Lake Dr.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5318603, -82.6526062)"
+  },
+  {
+    "Address Number": "205 Fennel Dun Cr.",
+    "AT&T Internet 5 to 1000": "Intenet 100",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5353019, -82.6490616)"
+  },
+  {
+    "Address Number": "209 Fennel Dun Cr",
+    "AT&T Internet 5 to 1000": "Internet 75",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5350467, -82.6490616)"
+  },
+  {
+    "Address Number": "312 Fennel Dun Cr.",
+    "AT&T Internet 5 to 1000": "Intenet 100",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5369776, -82.643069)"
+  },
+  {
+    "Address Number": "323 Fennel Dun Cr.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5381245, -82.642748)"
+  },
+  {
+    "Address Number": "417  Mountain Was",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5335808, -82.6462975)"
+  },
+  {
+    "Address Number": "430 Mountain Wasp",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.532666, -82.646091)"
+  },
+  {
+    "Address Number": "5 Kingsmill Ct.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5357303, -82.65268929999999)"
+  },
+  {
+    "Address Number": "8 Kingsmill Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5361832, -82.65235299999999)"
+  },
+  {
+    "Address Number": "18 Kingsmill Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5366291, -82.6526906)"
+  },
+  {
+    "Address Number": "25 Kingsmill Ct.",
+    "AT&T Internet 5 to 1000": "Internet 25",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.536849, -82.65345169999999)"
+  },
+  {
+    "Address Number": "44 Kingsmill Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5378586, -82.6528594)"
+  },
+  {
+    "Address Number": "4 Hoolet Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5389286, -82.64855519999999)"
+  },
+  {
+    "Address Number": "17 Hoolet Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.538081, -82.649394)"
+  },
+  {
+    "Address Number": "5 Hoolet Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5391955, -82.6491696)"
+  },
+  {
+    "Address Number": "105 Damp Crane Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5382527, -82.64828090000002)"
+  },
+  {
+    "Address Number": "107 Ducks Dun Ct.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5372121, -82.6455168)"
+  },
+  {
+    "Address Number": "208 Ducks Dun Ct.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5374775, -82.645388)"
+  },
+  {
+    "Address Number": "111 Harrop Dun Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5357216, -82.6444406)"
+  },
+  {
+    "Address Number": "133 Harrop Dun Ct.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.534601, -82.6431534)"
+  },
+  {
+    "Address Number": "26 Sparkle Dun Dr.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5358779, -82.64207119999999)"
+  },
+  {
+    "Address Number": "42 Sparkle Dun Dr.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5367767, -82.6409749)"
+  },
+  {
+    "Address Number": "129 Lake Dr.",
+    "AT&T Internet 5 to 1000": "Internet 25",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.585153, -82.46628299999999)"
+  },
+  {
+    "Address Number": "128 Lake Dr.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.531888, -82.652606)"
+  },
+  {
+    "Address Number": "122 Lake Dr.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5321806, -82.65262729999999)"
+  },
+  {
+    "Address Number": "1013 Coves Pheasant",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.53161, -82.654355)"
+  },
+  {
+    "Address Number": "1010 Coves Pheasant",
+    "AT&T Internet 5 to 1000": "Internet 25",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5314168, -82.6537033)"
+  },
+  {
+    "Address Number": "1009 Coves Pheasant",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.531127, -82.654396)"
+  },
+  {
+    "Address Number": "1011 Coves Pheasant",
+    "AT&T Internet 5 to 1000": "Internet 25",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.531368, -82.65459)"
+  },
+  {
+    "Address Number": "724 Wickhams Fancy",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.530851, -82.641037)"
+  },
+  {
+    "Address Number": "715 Wichamns Fancy",
+    "AT&T Internet 5 to 1000": "Internet 5 Mbps",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.531205, -82.64242)"
+  },
+  {
+    "Address Number": "749 Wickhams Fancy",
+    "AT&T Internet 5 to 1000": "Internet 5 Mbps",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.53287, -82.641282)"
+  },
+  {
+    "Address Number": "12 Pearly Wickham",
+    "AT&T Internet 5 to 1000": "Internet 10 Mbps",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.52932, -82.643807)"
+  },
+  {
+    "Address Number": "48 Pearly Wickham",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.528553, -82.646254)"
+  },
+  {
+    "Address Number": "18 S. Kaufman Stone Way",
+    "AT&T Internet 5 to 1000": "NA",
+    "Charter Speed 100 + & 200+": 100,
+    "Coords": "(35.527085, -82.6634179)"
+  },
+  {
+    "Address Number": "22 S. Kaufman Stone Way",
+    "AT&T Internet 5 to 1000": "NA",
+    "Charter Speed 100 + & 200+": 100,
+    "Coords": "(35.527085, -82.6634179)"
+  },
+  {
+    "Address Number": "67 Orvis Stone Cir.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.530915, -82.657513)"
+  },
+  {
+    "Address Number": "129 Orvis Stone Cir.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.530868, -82.6626129)"
+  },
+  {
+    "Address Number": "71 Orvis Stone Cir.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.530654, -82.657498)"
+  },
+  {
+    "Address Number": "2 Gosling Ct.",
+    "AT&T Internet 5 to 1000": "Internet 25",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5302383, -82.6521632)"
+  },
+  {
+    "Address Number": "7 Gosling Ct.",
+    "AT&T Internet 5 to 1000": "Internet 10",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5310153, -82.6520704)"
+  },
+  {
+    "Address Number": "14 White Palmer Ct.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.530586, -82.647786)"
+  },
+  {
+    "Address Number": "8 White Palmer Ct.",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5303198, -82.6486185)"
+  },
+  {
+    "Address Number": "2 Grouise Wing Ct.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.529413, -82.647431)"
+  },
+  {
+    "Address Number": "25 Grouse Wing Ct.",
+    "AT&T Internet 5 to 1000": "Internet Basic 5",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5291378, -82.6494625)"
+  },
+  {
+    "Address Number": "50 Black Horse Run",
+    "AT&T Internet 5 to 1000": "Internet 50",
+    "Charter Speed 100 + & 200+": "Charter Currenet",
+    "Coords": "(35.5275311, -82.6550012)"
+  },
+  {
+    "Address Number": "14 Black Horse Run",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": "Charter Currenet",
+    "Coords": "(35.5265401, -82.6567218)"
+  },
+  {
+    "Address Number": "3 Moser  Sedge",
+    "AT&T Internet 5 to 1000": "NA",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5274454, -82.6593785)"
+  },
+  {
+    "Address Number": "17 Moser Sedge",
+    "AT&T Internet 5 to 1000": "Internet 5 Mbps",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5278408, -82.6578385)"
+  },
+  {
+    "Address Number": "21 Gray Duster Cir.",
+    "AT&T Internet 5 to 1000": "Intenet Basic 5",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.525304, -82.6580916)"
+  },
+  {
+    "Address Number": "88  Gray Duter Cir.",
+    "AT&T Internet 5 to 1000": "Intenet Basic 5",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5257047, -82.6552435)"
+  },
+  {
+    "Address Number": "3 Cats Whisker Dr.",
+    "AT&T Internet 5 to 1000": "Internet Basic 5",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5248739, -82.6552224)"
+  },
+  {
+    "Address Number": "36 Cats Whisker Dr.",
+    "AT&T Internet 5 to 1000": "Internet Basic 5",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.5264509, -82.6556233)"
+  },
+  {
+    "Address Number": "15 Hares Ears",
+    "AT&T Internet 5 to 1000": "Internet Basic 5",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.524326, -82.653637)"
+  },
+  {
+    "Address Number": "22 Estatoe Gap Rd.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5199064, -82.6589283)"
+  },
+  {
+    "Address Number": "8 Estatoe Gap Rd.",
+    "AT&T Internet 5 to 1000": "ATT Current",
+    "Charter Speed 100 + & 200+": 200,
+    "Coords": "(35.5209901, -82.6590588)"
+  },
+  {
+    "Address Number": "6 Spencer Ridge",
+    "AT&T Internet 5 to 1000": "Internet 1000",
+    "Charter Speed 100 + & 200+": "Charter Current",
+    "Coords": "(35.521941, -82.65902)"
+  }
+];
+
+var map = L.map('map', {
+    // Set latitude and longitude of the map center (required)
+    center: [35.532, -82.643], 
+    // Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
+    zoom: 15,
+    maxZoom: 18,
+	minZoom: 10,
+//	maxBounds: bounds			//Uncomment to activate binding window to survey area
+
+});
+
+L.control.scale().addTo(map);
+$(".leaflet-control-zoom").css("visibility","hidden");
+// Create a Tile Layer and add it to the map
+
+//Create access token linking to the map on a mappbox account
+survey_results = L.tileLayer('https://api.mapbox.com/styles/v1/westngn/ck2z53u892diw1cldqewni6bg/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2VzdG5nbiIsImEiOiJjanVzc3I1Zm8wMG5mNDNrZnl1aGt5cmhvIn0.0AULTTBaoxcqci1Iie9IDA', {
+}).addTo(map);
+
+var markers = new L.markerClusterGroup();
+
+markers.on('clusterclick', function (a) {
+    a.layer.zoomToBounds({padding: [20,20]});
+});
+
+//making a new icon
+var PinIcon = L.Icon.extend({
+    options: {
+       iconSize:     [20, 20],
+       iconAnchor:   [10, 19],
+       popupAnchor:  [0, -10]
+    }
+});
+
+var grayIcon = new PinIcon({
+    iconUrl: 'img/pin_gray.png', 
+});
+
+var service_markers = [];
+
+var serviceSubGroup = L.featureGroup.subGroup(markers, service_markers);
+
+var grayPopup = L.popup();
+
+function makeServiceMarkers(thing){
+	for(var i = 0; i<thing.length; i++){
+		//this creates a new variable just for the coords of the location. 
+		//It splits the coords into X and Y based on the REGEX (Parens, Commas, Empty Space) below. 
+		//".filter(Boolean)" deletes empty entries created by the split delimiting.  
+		var splitCoords = (thing[i]["Coords"]).split(/[ ,()]+/).filter(Boolean);
+		var service_marker = L.marker(new L.LatLng(splitCoords[0],splitCoords[1]), {
+			icon: grayIcon
+		});
+
+	service_marker.bindPopup("AT&T: " + thing[i]["AT&T Internet 5 to 1000"] + "<br>"
+		+"Charter: "+ thing[i]["Charter Speed 100 + & 200+"]);
+
+	service_markers.push(service_marker);	
+	}
+
+	serviceSubGroup = L.featureGroup.subGroup(markers, service_markers);
+
+}
+
+makeServiceMarkers(servicelatlngs);
+map.addLayer(serviceSubGroup);
+map.addLayer(markers);
+
