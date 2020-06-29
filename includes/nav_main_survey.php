2541fc43cd5699b734fe722b9c9dd01d3b63e810
Elias Landsman <elandsma@unca.edu>
2020-06-29 19:30:46 -0400

    adding include folder to git

---
+++ includes/nav_main_survey.php
@@ -0,0 +1,31 @@
+   <!-- Navigation -->
+	<nav class="navbar navbar-expand-lg bg-dblue navbar-dark mainnav">
+     	<a class="navbar-brand" href="https://wncbroadband.org/blog">
+        	<img src="../../img/wncbroadbandlogo.png" alt="WNC Broadband Project Logo Image and Link" class="img-fluid logo">
+      	</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        	<span class="navbar-toggler-icon"></span>
+      	</button>
+		<div class="collapse navbar-collapse navbars pull-right" id="navbar1">
+        	<ul class="navbar-nav ml-auto small">
+          		<li class="nav-item">
+            		<a class="nav-link" href="https://wncbroadband.org/blog">Home</a>
+          		</li>
+              	<li class="nav-item">
+                	<a class="nav-link" href="../../aboutproject.html">About</a>
+              	</li>
+              	<li class="nav-item">
+               		<a class="nav-link" href="../../broadband101.html">Broadband 101</a>
+              	</li>
+              	<li class="nav-item">
+                	<a class="nav-link" href="https://wncbroadband.org/blog/community-initiatives/">Community Initiatives</a>
+              	</li>
+              	<li class="nav-item">
+            		<a class="nav-link" href="https://wncbroadband.org/blog/advocacy/">Actions We Can Take</a>
+          		</li>
+          		<li class="nav-item">
+            		<a class="nav-link" href="https://wncbroadband.org/blog/blog/">News</a>
+          		</li>
+        	</ul>
+  		</div>
+	</nav>
