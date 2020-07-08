2144ba256edc986b6188031829fd76636f0f818f
Elias Landsman <elandsma@unca.edu>
2020-07-08 19:53:51 -0400

    added privacypolicy.php to git

---
+++ privacypolicy.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta name="description" content="">
+  	<meta name="author" content="">
+  	<title>WNC Broadband Project | Privacy Policy</title>
+ 	<!-- Bootstrap core CSS -->
+  	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  	<!-- JQuery -->
+  	<script src="js/jquery.min.js"></script> 
+ 	<!-- Bootstrap core CSS -->
+  	<link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  	<!-- Custom fonts for this template -->
+  	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  	<!--Site Stylesheet-->
+  	<link href="css/style.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap core JavaScript -->
+  	<script src="vendor/jquery/jquery.slim.min.js"></script>
+  	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>  
+</head>
+<body>
+	<!-- Navigation -->
+	<?php include("http://www.wncbroadband.org/includes/nav_main_core.php");?>
+
+	<section class="header pt-5">  
+  		<div class="container pt-5">
+  			<h1>Welcome to our Privacy Policy</h1>
+			<h4>Your privacy is critically important to us.</h4>
+			WNC Broadband Project is located at:<br/>
+			<address>
+			WNC Broadband Project<br/>Asheville <br />- NC , United States<br/>			</address>
+
+			<p>It is WNC Broadband Project's policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to <a href="https://wncbroadband.org/">https://wncbroadband.org/</a> (hereinafter, "us", "we", or "https://wncbroadband.org/"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.</p>
+			<p>This Privacy Policy, together with the Terms and conditions posted on our Website, set forth the general rules and policies governing your use of our Website. Depending on your activities when visiting our Website, you may be required to agree to additional terms and conditions.</p>
+
+			<h3>Website Visitors</h3>
+			<p>Like most website operators, WNC Broadband Project collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. WNC Broadband Project's purpose in collecting non-personally identifying information is to better understand how WNC Broadband Project's visitors use its website. From time to time, WNC Broadband Project may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.</p>
+			<p>WNC Broadband Project also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on https://wncbroadband.org/ blog posts. WNC Broadband Project only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below.</p>
+			
+			<h3>Gathering of Personally-Identifying Information</h3>
+			<p>Certain visitors to WNC Broadband Project's websites choose to interact with WNC Broadband Project in ways that require WNC Broadband Project to gather personally-identifying information. The amount and type of information that WNC Broadband Project gathers depends on the nature of the interaction. For example, we ask visitors who sign up for a blog at https://wncbroadband.org/ to provide a username and email address.</p>
+			
+			<h3>Security</h3>
+			<p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+
+			<h3>Links To External Sites</h3>
+			<p>Our Service may contain links to external sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy and terms and conditions of every site you visit.</p>
+			<p>We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites, products or services.</p>
+			
+			<h3>Aggregated Statistics</h3>
+			<p>WNC Broadband Project may collect statistics about the behavior of visitors to its website. WNC Broadband Project may display this information publicly or provide it to others. However, WNC Broadband Project does not disclose your personally-identifying information.</p>
+			
+			<h3>Cookies</h3>
+			<p>To enrich and perfect your online experience, WNC Broadband Project uses "Cookies", similar technologies and services provided by others to display personalized content, appropriate advertising and store your preferences on your computer.</p>
+			<p>A cookie is a string of information that a website stores on a visitor's computer, and that the visitor's browser provides to the website each time the visitor returns. WNC Broadband Project uses cookies to help WNC Broadband Project identify and track visitors, their usage of https://wncbroadband.org/, and their website access preferences. WNC Broadband Project visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using WNC Broadband Project's websites, with the drawback that certain features of WNC Broadband Project's websites may not function properly without the aid of cookies.</p>
+			<p>By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to WNC Broadband Project's use of cookies.</p>
+			
+			<h3>Privacy Policy Changes</h3>
+			<p>Although most changes are likely to be minor, WNC Broadband Project may change its Privacy Policy from time to time, and in WNC Broadband Project's sole discretion. WNC Broadband Project encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.</p>
+			
+			<h3>Credit &amp; Contact Information</h3>
+			<p>This privacy policy was created at <a style="color:inherit;text-decoration:none;" href="https://termsandconditionstemplate.com/privacy-policy-generator/" title="Privacy policy template generator" target="_blank">termsandconditionstemplate.com</a>. If you have any questions about this Privacy Policy, please contact us via <a href="mailto:WNCBroadbandProject@gmail.com">email</a>.</p>
+
+		</div>
+	</section>
+
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+</body>
+</html>
+					
