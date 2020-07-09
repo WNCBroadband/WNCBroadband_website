a8c28b3965b29d93495ee0692d1786ed5d8d7b50
Elias Landsman <elandsma@unca.edu>
2020-07-08 20:15:05 -0400

    Created donate.php. Content needs editing.

---
+++ donate.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta name="description" content="">
+  	<meta name="author" content="">
+  	<title>WNC Broadband Project | FAQ</title>
+  	<!-- Bootstrap core CSS -->
+  	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  	<!-- JQuery -->
+  	<script src="js/jquery.min.js"></script> 
+  	<!-- Bootstrap core CSS -->
+  	<link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  	<!-- Custom fonts for this template -->
+  	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  	<!--Site Stylesheet-->
+  	<link href="css/style.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap core JavaScript -->
+  	<script src="vendor/jquery/jquery.slim.min.js"></script>
+  	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>  
+</head>
+
+<body>
+
+	<!-- Navigation -->
+	<?php include("http://www.wncbroadband.org/includes/nav_main_core.php");?>
+
+ 	<!-- Page Content -->
+	<section class="header bg-broad pt-5">  
+  		<div class="container pt-5">
+  			<div class="row">
+  				<div class="col-lg-7">
+  					<h1>Donate to the WNC Broadband Project</h1>
+  				</div>
+  			</div>
+  		</div>
+	</section>
+	
+	<section class="p-5">
+		<div class="container">
+	  		<h2>How to donate</h2>
+	  		<p class="lead">Leading information about donating</p>
+	  		<p>More detailed information about donating.</p>
+	  		<a class="btn btn-primary mb-5" href="https://wncbroadband.org/blog/contact/">Button link somewhere &gt;</a>
+	  		&nbsp;&nbsp;&nbsp;
+	  		<a class="btn btn-primary mb-5" href="https://wncbroadband.org/blog/advisory/">Button link somewhere else &gt;</a>
+		</div>
+	</section>
+	<section class="p-5 bg-light">
+  		<div class="container">
+  			<h2>Another section about donating</h2>
+  				<h4>Here is more information.</h4>
+  					<p>Lorem ipsum dolor sit amet</p>
+  					<p>UT enim ad mimim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  					<h5 class="mt-3">Contact Information</h5>
+					  <div class="row mb-5">
+					  	<div class="col-sm-4">
+					    	<p class="lead mb-0">William Sederburg</p>
+					      	<p>Chair<br>
+					      	<a href="mailto:wsederburg@gmail.com">wsederburg@gmail.com</a></p>
+					    </div>
+					    
+					    <div class="col-sm-4">
+					      <p class="lead mb-0">Sara Nichols</p>
+					      <p>Land of Sky<br>
+					      <a href="mailto:sara@landofsky.org">sara@landofsky.org</a></p>
+					    </div>
+					    
+					    <div class="col-sm-4">
+					      <p class="lead mb-0">Stagg Newman</p>
+					      <p>Technical Advisor<br>
+					      <a href="mailto:lsnewmanjr@yahoo.com">lsnewmanjr@yahoo.com</a></p>
+					    </div>					    
+				</div>
+  			</div>
+  		</div>
+	</section>
+
+
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+
+</html>
