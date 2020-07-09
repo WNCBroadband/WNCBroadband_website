857967bc72788027112aa0275fd25859a603f849
Elias Landsman <elandsma@unca.edu>
2020-07-09 14:33:57 -0400

    changed links in nav include files to point to php. Made all paths absolute.

---
+++ includes/nav_main_core.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dblue fixed-top stroke">
+	<div class="container">
+    	<a class="navbar-brand" href="https://wncbroadband.org/blog/">
+        	<img src="https://wncbroadband.org/img/wncbroadbandlogo.png" width="100px" alt="WNC Broadband Project Logo Image and Link">
+    	</a>
+    	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      		<span class="navbar-toggler-icon"></span>
+      	</button>
+      	<div class="collapse navbar-collapse" id="navbarResponsive">
+          	<ul class="navbar-nav ml-auto">
+            	<li class="nav-item">
+                	<a class="nav-link" href="https://wncbroadband.org/blog/">Home</a>
+              	</li>
+              	<li class="nav-item"><a class="nav-link" href="https://wncbroadband.org/aboutproject.php">About</a>
+              	</li>
+              	<li class="nav-item">
+                	<a class="nav-link" href="https://wncbroadband.org/broadband101.php">Broadband 101</a>
+              	</li>
+               	<li class="nav-item">
+                	<a class="nav-link" href="https://wncbroadband.org/blog/community-initiatives/">Community Initiatives</a>
+              	</li>
+               	<li class="nav-item">
+                	<a class="nav-link" href="https://wncbroadband.org/blog/advocacy/">Actions We Can Take</a>
+              	</li>
+              	<li class="nav-item">
+                	<a class="nav-link" href="https://wncbroadband.org/blog/blog/">News</a>
+              	</li>
+            </ul>
+    	</div>
+	</div>
+</nav>
+
+<!-- add "active" class any to nav-link item for it to be white. For instance: -->
+<!-- <a class="nav-link active" href="broadband101.html">Broadband 101</a> -->
--- includes/nav_main_survey.php
+++ includes/nav_main_survey.php
@@ -1,7 +1,7 @@
    <!-- Navigation -->
 	<nav class="navbar navbar-expand-lg bg-dblue navbar-dark mainnav">
      	<a class="navbar-brand" href="https://wncbroadband.org/blog">
-        	<img src="../../img/wncbroadbandlogo.png" alt="WNC Broadband Project Logo Image and Link" class="img-fluid logo">
+        	<img src="https://wncbroadband.org/img/wncbroadbandlogo.png" alt="WNC Broadband Project Logo Image and Link" class="img-fluid logo">
       	</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         	<span class="navbar-toggler-icon"></span>
@@ -12,10 +12,10 @@
             		<a class="nav-link" href="https://wncbroadband.org/blog">Home</a>
           		</li>
               	<li class="nav-item">
-                	<a class="nav-link" href="../../aboutproject.html">About</a>
+                	<a class="nav-link" href="https://wncbroadband.org/aboutproject.php">About</a>
               	</li>
               	<li class="nav-item">
-               		<a class="nav-link" href="../../broadband101.html">Broadband 101</a>
+               		<a class="nav-link" href="https://wncbroadband.org/broadband101.php">Broadband 101</a>
               	</li>
               	<li class="nav-item">
                 	<a class="nav-link" href="https://wncbroadband.org/blog/community-initiatives/">Community Initiatives</a>
