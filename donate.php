47569c1093593f1d6dd9f009df8479b807826d49
Elias Landsman <elandsma@unca.edu>
2020-07-16 17:24:12 -0400

    added some donate.php content.

--- donate.php
+++ donate.php
@@ -51,37 +51,66 @@
 	  		<a class="btn btn-primary mb-5" href="https://wncbroadband.org/blog/advisory/">Button link somewhere else &gt;</a>
 		</div>
 	</section>
-	<section class="p-5 bg-light">
-  		<div class="container">
-  			<h2>Another section about donating</h2>
-  				<h4>Here is more information.</h4>
-  					<p>Lorem ipsum dolor sit amet</p>
-  					<p>UT enim ad mimim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  					<h5 class="mt-3">Contact Information</h5>
-					  <div class="row mb-5">
-					  	<div class="col-sm-4">
-					    	<p class="lead mb-0">William Sederburg</p>
-					      	<p>Chair<br>
-					      	<a href="mailto:wsederburg@gmail.com">wsederburg@gmail.com</a></p>
-					    </div>
-					    
-					    <div class="col-sm-4">
-					      <p class="lead mb-0">Sara Nichols</p>
-					      <p>Land of Sky<br>
-					      <a href="mailto:sara@landofsky.org">sara@landofsky.org</a></p>
-					    </div>
-					    
-					    <div class="col-sm-4">
-					      <p class="lead mb-0">Stagg Newman</p>
-					      <p>Technical Advisor<br>
-					      <a href="mailto:lsnewmanjr@yahoo.com">lsnewmanjr@yahoo.com</a></p>
-					    </div>					    
-				</div>
-  			</div>
-  		</div>
+	
+	<section class="p-5">
+    	<div class="container">
+    		<h2>Donate today</h2>
+    		<p class="lead">Financial Donations</p>
+    		<p>Donate today to the WNC Broadband Project, a member of the UNCA Foundation.</p>
+    		<p>Your financial support helps Western North Carolina by:</p>
+    		<ul>
+    			<li>Reducing the digital divide</li>
+    			<li>Educating community members</li>
+    			<li>Engaging WNC students</li>
+    			<li>Improving broadband access</li>
+    		</ul>
+    		<a class="btn btn-primary mb-5" href="https://wncbroadband.org/donate.php">Donate Today &gt;</a>		
+		</div>
 	</section>
+	
+	<section class="p-5 bg-light">
+		<div class="container">
+			<h2>Hardware Donations</h2>
+			<p>Goodwill of Western North Carolina has a partnership with Dell Reconnect in which they are able to recycle 100% of donated electronics, including printers and computer peripherals.</p>
+			
+			<p>Dell Reconnect is opetated in partnership between Dell and Goodwill, providing free drop-off recycling to allow consumers to responsibly recycle any brand of used computer equipment, in any condition, at more than 2,000 participating Goodwill locations in the U.S.</p>
+			<p>Recent model computers are refurbished and resold, giving local families a change to buy coputers and other hardware at a great price while providing revenue for Goodwill's employment and training services</p>
+			<p>Some computers may require extra preparation before they are donated, so remember to remove your personal data from hard drives or other storage media before bringing your equipment to Goodwill</p>
+			<p>Learn more about the Dell Reconnect program by visiting <a href="http://www.goodwillpc.org" target="_blank">www.goodwillpc.org</a></p>
+		</div>
+	</section>
+	
+<!-- 	<section class="p-5 bg-light"> -->
+<!--   		<div class="container"> -->
+<!--   			<h2>Another section about donating</h2> -->
+<!--   				<h4>Here is more information.</h4> -->
+<!--   					<p>Lorem ipsum dolor sit amet</p> -->
+<!--   					<p>UT enim ad mimim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
+<!--   					<h5 class="mt-3">Contact Information</h5> -->
+<!-- 					  <div class="row mb-5"> -->
+<!-- 					  	<div class="col-sm-4"> -->
+<!-- 					    	<p class="lead mb-0">William Sederburg</p> -->
+<!-- 					      	<p>Chair<br> -->
+<!-- 					      	<a href="mailto:wsederburg@gmail.com">wsederburg@gmail.com</a></p> -->
+<!-- 					    </div> -->
+					    
+<!-- 					    <div class="col-sm-4"> -->
+<!-- 					      <p class="lead mb-0">Sara Nichols</p> -->
+<!-- 					      <p>Land of Sky<br> -->
+<!-- 					      <a href="mailto:sara@landofsky.org">sara@landofsky.org</a></p> -->
+<!-- 					    </div> -->
+					    
+<!-- 					    <div class="col-sm-4"> -->
+<!-- 					      <p class="lead mb-0">Stagg Newman</p> -->
+<!-- 					      <p>Technical Advisor<br> -->
+<!-- 					      <a href="mailto:lsnewmanjr@yahoo.com">lsnewmanjr@yahoo.com</a></p> -->
+<!-- 					    </div>					     -->
+<!-- 				</div> -->
+<!--   			</div> -->
+<!--   		</div> -->
+<!-- 	</section> -->
 
 
 	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
-
+</body>
 </html>
