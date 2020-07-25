1fb8903f56cb89ac556256e2dad30ab3624c7fdc
Elias Landsman <elandsma@unca.edu>
2020-07-25 15:40:58 -0400

    added contact.php to git

---
+++ contact.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- <meta http-equiv="refresh" content="0;URL=https://wncbroadband.org/blog/contact/"> -->
+<!-- Old code to redirect to workpress page -->
+
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+  	<meta name="author" content="">
+  	<title>WNC Broadband Project | Contact</title>
+
+	<!-- Bootstrap core CSS -->
+  	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  	<!-- JQuery -->
+  	<script src="js/jquery.min.js"></script>
+  	<!-- Bootstrap core CSS -->
+  	<link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  	<!-- Custom fonts for this template -->
+  	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  	<!--Site Stylesheet-->
+  	<link href="css/style.css" rel="stylesheet" type="text/css">
+  	<!-- Bootstrap core JavaScript -->
+	<script src="vendor/jquery/jquery.slim.min.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</head>
+
+<body>
+	<!-- Navigation -->
+  	<nav class="navbar navbar-expand-lg navbar-dark bg-dblue fixed-top stroke">
+    	<div class="container">
+	      	<a class="navbar-brand" href="https://wncbroadband.org/blog/">
+	        	<img src="img/wncbroadbandlogo.png" width="100px" alt="WNC Broadband Project Logo Image and Link">
+	      	</a>
+	      	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	        	<span class="navbar-toggler-icon"></span>
+	      	</button>
+	      	<div class="collapse navbar-collapse" id="navbarResponsive">
+	        	<ul class="navbar-nav ml-auto">
+	          		<li class="nav-item">
+	            		<a class="nav-link" href="https://wncbroadband.org/blog/">Home</a>
+	          		</li>
+	          		<li class="nav-item"><a class="nav-link" href="aboutproject.html">About</a>
+		          	</li>
+		          	<li class="nav-item">
+		            	<a class="nav-link" href="broadband101.html">Broadband 101</a>
+		          	</li>
+		           	<li class="nav-item">
+		            	<a class="nav-link" href="https://wncbroadband.org/blog/community-initiatives/">Community Initiatives</a>
+		          	</li>
+		           	<li class="nav-item">
+		            	<a class="nav-link" href="advocacy.html">Actions We Can Take</a>
+		          	</li>
+		          	<li class="nav-item">
+		            	<a class="nav-link" href="https://wncbroadband.org/blog/blog/">News</a>
+		          	</li>
+	        	</ul>
+			</div>
+    	</div>
+	</nav>
+
+	<!-- Page Content -->
+	<section class="header bg-broad pt-5">
+		<div class="container pt-5">
+			<div class="row">
+	  			<div class="col-lg-7">
+	  				<h1>Contact</h1>
+	  			</div>
+	  		</div>
+	  	</div>
+	</section>
+
+	<section class="bg-light pt-5 pb-5">
+  		<div class="container contacts">
+  			<h2>Project Contacts</h2>
+		    <div class="row">
+		    	<div class="col-lg-4 col-md-6 mb-4">
+		        	<img src="img/billsederburg.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block contact-photo"/>
+		        	<h5>Dr. William Sederburg</h5>
+		          	<p class="font-weight-light mb-0">Retired College President<br>
+		            Chair, WestNGN<br>
+		            <a href="mailto:wsederburg@gmail.com">wsederburg@gmail.com</a>
+		      	</div>
+		      	<div class="col-lg-4 col-md-6 mb-4">
+		        	<img src="img/staggnewman.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block  contact-photo"/>
+		        	<h5>Mr. Stagg Newman</h5>
+		          	<p class="font-weight-light mb-0">Retired FCC Chief Technologist<br>
+		            Chair of WestNGN Local App Tool<br>
+		            <a href="mailto:lsnewmanjr@yahoo.com">lsnewmanjr@yahoo.com</a></p>
+		      	</div>
+		      	<div class="col-lg-4 col-md-6 mb-4">
+		        	<img src="img/edkatz.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block contact-photo"/>
+		        	<h5>Dr. Edward Katz</h5>
+		          	<p class="font-weight-light mb-0">UNCA Faculty<br>
+		            Professor of English<br>
+		            University Project Coordinator<br>
+		            <a href="mailto:ekatz@unca.edu">ekatz@unca.edu</a></p>
+		      	</div>
+		      	<div class="col-lg-4 col-md-6 mb-4">
+		        	<img src="img/briandrawert.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block contact-photo"/>
+		       		<h5>Dr. Brian Drawert</h5>
+		          	<p class="font-weight-light mb-0">UNCA Faculty<br>
+		            Assistant Professor of Computer Science<br>
+		            Technical Advisor<br>
+		            <a href="mailto:bdrawert@unca.edu">bdrawert@unca.edu</a></p>
+		        </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+              <img src="img/FrankDiPalma.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block contact-photo"/>
+            <h5>Frank DiPalma</h5>
+               <p class="font-weight-light mb-0"> Retired Utility Executive and Consultant<br>
+               <br>
+                 <br>
+                 <a href="mailto:fdipalma2000@gmail.com">fdipalma2000@gmail.com</a></p>
+             </div>
+           <div class="col-lg-4 col-md-6 mb-4">
+             <img src="img/marc-czarnecki.png" class="img-fluid rounded-circle mb-3 mx-auto d-block contact-photo"/>
+            <h5>Marc Czarnecki</h5>
+               <p class="font-weight-light mb-0"><br>
+               <br>
+                Social Media Coordinator<br>
+                 <a href="mailto:marc@wncbroadband.org">marc@wncbroadband.org</a></p>
+             </div>
+           <div class="col-lg-4 col-md-6 mb-4">
+             <img src="img/Anitra-Griffin.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block contact-photo"/>
+            <h5>Anitra Griffin</h5>
+               <p class="font-weight-light mb-0">UNCA Student<br>
+                 Web Design Intern<br>
+                 <a href="mailto:agriffi@unca.edu">agriffi1@unca.edu</a></p>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-4">
+               <img src="img/Elias.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block contact-photo"/>
+              <h5>Elias Landsman</h5>
+                 <p class="font-weight-light mb-0">UNCA Student<br>
+                   <br>
+                   Web Development Intern<br>
+                   <a href="mailto:elandsma@unca.edu">elandsma@unca.edu</a></p>
+               </div>
+    		</div>
+  		</div>
+	</section>
+
+	<!--Advisory Team-->
+	<section class="bg-light pt-5 pb-5">
+  	<div class="container contacts">
+  		<h2>Project Partners</h2>
+    		<div class="row">
+      			<div class="col-lg-4 col-md-6 mb-4">
+       				<!--<img src="img/saranichols.jpg" class="img-fluid rounded-circle mb-3 mx-auto d-block  contact-photo"/>-->
+        			<h5 class="mt-3">Ms. Sara Nichols</h5>
+          			<p class="font-weight-light mb-0">Land of Sky Planner<br>
+            		Leader of Four County Survey Project<br>
+            		<a href="mailto:sara@landofsky.org">sara@landofsky.org</a></p>
+      			</div>
+    		</div>
+  		</div>
+	</section>
+
+	<section class="pt-5 pb-5">
+		<div class="container contacts">
+			<h2>Advisory Committee</h2>
+			<p class="lead">View the list of Advisory Committee members <a href="advisory.html">here</a>.</p>
+		</div>
+	</section>
+
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+
+</body>
+
+</html>
