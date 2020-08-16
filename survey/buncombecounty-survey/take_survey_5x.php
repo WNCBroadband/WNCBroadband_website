6f87a2b3532f4b9bff27e48fba59ee486c19f0b6
Elias Landsman <elandsma@unca.edu>
2020-08-16 15:42:46 -0400

    buncombe county survey front-end fixes

--- survey/buncombecounty-survey/take_survey_5x.php
+++ survey/buncombecounty-survey/take_survey_5x.php
@@ -183,7 +183,7 @@ include('../../db.php');
           			<br><br>
           			<div class="q-break"></div><br>
             		<h4>11&#41; There are major issues in broadband delivery concerning where broadband is available and what the actual speeds are delivered by providers. We are working on ways to determine the speeds in your area. Please help us by using the following speed test</h4><br>
-            		<p><a href="https://wncbroadband.org/blog/speedtest-best-practices/" target="_blank">click here for information on how to get a more accurate speed test</a></p><br>
+            		<p><a href="https://wncbroadband.org/blog/speedtest-best-practices/" target="_blank" style="color: black; text-decoration: underline">click here for information on how to get a more accurate speed test</a></p><br>
 					
 					<div id="speedtestArea">
 						<?php include "speedtest5x.php"?>
@@ -203,13 +203,13 @@ include('../../db.php');
             	    </script>            
             		<br><br>
             		<h4>We recommend taking the test more than once. The best results from your speed tests will be saved.</h4><br>
-                    <h5>Your Best Results from the Speed Test:</h5>
-                    <div class="slidecontainer">
+                    <h5 class="text-center">Your Best Results from the Speed Test:</h5>
+<!--                     <div class="slidecontainer"> -->
            			   <p class="text-center lead">Download Speed <input id="downtextbox" readonly type="text" class="js-input" name="SPEEDTEST__libre__download" class="text" maxlength="5" size="5"> Mbps</p>
-            		</div>
-            		<div class="slidecontainer">
+<!--             		</div> -->
+<!--             		<div class="slidecontainer"> -->
              			<p class="text-center lead">Upload Speed <input id="uptextbox" readonly type="text" class="js-input" name="SPEEDTEST__libre__upload" class="text" maxlength="5" size="5"> Mbps</p>
-            		</div>
+<!--             		</div> -->
             		<br><br>
            			<h4>Based on the results of the speed test, your download speed allows you the following capabilities of various applications:</h4><br>
                     <div class="slidecontainer">
