7e327738304d6c941056af9a66b1cd33fd93c3fe
Elias Landsman <elandsma@unca.edu>
2020-09-06 15:26:44 -0400

    added haywood county survey page

---
+++ survey/haywoodcounty-survey/q1.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>WNC Broadband Project | Survey</title>
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- JQuery -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.min.js"></script> 
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!-- Custom fonts for this template -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	<!--Site Stylesheet-->
+	<link href="https://wncbroadband.org/css/style.css" rel="stylesheet" type="text/css">
+	<!-- Bootstrap core JavaScript -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.slim.min.js"></script>
+	<script src="https://wncbroadband.org/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+	<!-- Navigation -->
+	<?php include("http://www.wncbroadband.org/includes/nav_main_survey.php");?>
+
+
+<!-- <nav class="navbar navbar-expand-md bg-lblue navbar-dark communitynav">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar2" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse navbars" id="navbar2">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="../../biltmorelake/index.html">Biltmore Lake</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../../biltmorelake/aboutBiltmoreLake.html">About Community</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="../../biltmorelake/survey.html">Survey</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../biltmorelake/providers.html">Service Providers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../biltmorelake/communitycontacts.html">Community Contacts</a>
+          </li>
+           <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Biltmore Lake Maps</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="../../biltmorelake/map/map.html">Speed Test Results</a></li>
+            <li><a class="dropdown-item" href="../../biltmorelake/map/map2.php">Services Offered</a></li>
+           </ul>
+          </li>
+        </ul>
+      </div>
+  </nav> -->
+
+	<section class="p-3 bg-light"></section>
+	<section class="p-5 bg-light">
+		<div id="survey-bg" class="row">
+			<div class="col-lg-6 mx-auto">
+				<div class="form-group">
+			    	<form class="form">
+			      		<h3>Does your household currently subscribe to Internet services?</h3>
+			      		<br>
+			      		<a class="btn btn-primary mt-auto mb-5" href="take_survey.php">Yes</a>
+			      		<br>
+			      		<a class="btn btn-primary mt-auto mb-5" href="survey-end.php"> No (or cell phone is the only source for your internet)</a>
+		        	</form>
+		      	</div>
+			</div>
+			<div class="my-auto showcase-text offset-lg-3 survey-bg col-lg-6">
+			</div>
+		</div>	
+	</section>
+	<div class="separate"></div>
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+</body>
+</html>
