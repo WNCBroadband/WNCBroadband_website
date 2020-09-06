251a89cdde14aa4f3c4c0dc60e85e457ad99e69a
Elias Landsman <elandsma@unca.edu>
2020-09-06 15:11:53 -0400

    advocacy.html -> advocacy.php

---
+++ advocacy.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>WNC Broadband Project | Actions We Can Take</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- JQuery -->
+  <script src="js/jquery.min.js"></script> 
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <!--Site Stylesheet-->
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.slim.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <!-- Navigation -->
+  	<script>
+ 		var navid = "actionswecantake";
+	//makes link active in nav
+	</script>
+	<?php include("http://www.wncbroadband.org/includes/nav_main_core.php");?>
+
+    <!-- Page Content -->
+	<section class="header bg-broad pt-5">  
+  		<div class="container pt-5">
+  			<div class="row">
+  				<div class="col-lg-7">
+  					<h1>Actions We Can Take</h1>
+  				</div>
+  			</div>
+  		</div>
+	</section>
+
+<!--
+<section class=" pt-4 pb-4 bg-blue">
+  <div class="container">
+    <div class="row">
+      
+      <div class="col-lg-4 col-sm-6 col-10 d-flex mx-auto m-2">
+          <a class="btn btn-primary mt-auto stretched-link p-3" href="#sec1"> <h4>WNC Broadband Speak Out &gt;</h4></a>
+      </div>
+
+      <div class="col-lg-4 col-sm-6 col-10 d-flex mx-auto m-2">
+          <a class="btn btn-primary mt-auto stretched-link p-3" href="#sec2"> <h4>What Candidates are Saying&gt;</h4></a>
+      </div>
+
+      <div class="col-lg-4 col-sm-6 col-10 d-flex mx-auto m-2">
+          <a class="btn btn-primary mt-auto stretched-link p-3" href="#sec3"> <h4>Steps You Can Take &gt;</h4></a>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<div class="separate"></div>
+-->
+
+    <!--WNC Broadband Speak Out-->
+	<section class="bg-light pt-5 pb-5">
+  		<div class="container" id="sec1">
+  			<div class="row">
+    			<div class="col-md-4">
+    				<a href="docs/BroadbandLeadershipSummit2020.pdf"><img class="img-fluid shadow" src="img/leadership_summary.png"></a>
+    			</div>
+    			<div class="col-md-7 pt-3">
+    				<h2>WNC Broadband Speak Out</h2>
+    				<p class="lead">Read about the broadband situation in Western North Carolina and actions that you, leaders, and schools can take to improve broadband. You can also view more resources from the Broadband Summit <a href="101links.html">here</a>.</p>
+    				<a href="docs/BroadbandLeadershipSummit2020.pdf" class="btn btn-primary">View PDF</a>
+    			</div>    
+  			</div>
+  		</div>
+	</section>
+
+	<div class="separate"></div>
+    
+    <!--What Leaders and Candidates are Saying-->
+	<section class="pt-5 pb-5">
+  		<div class="container" id="sec2">
+  			<h1 class="text-center">What Leaders and Candidates are Saying</h1>
+    
+            <!--Include Videos from Conference-->
+    		<div class="row">
+    			<div class="col-10 mx-auto">
+    				<br>
+                    <!--Video needs to have closed captioning-->
+                    <!--Full Video Link https://drive.google.com/file/d/1WnSl0glGjxCGBlB8JQ2pcjwoZIOjnpCo/preview-->
+                    <!--WLOS Coverage
+                    <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://sinclairstoryline.com/resources/embeds/jw8-embed.html?client=googima&file=https://content.uplynk.com/5a46cb02141b4742a560c87713687825.m3u8&autostart=false" allowfullscreen></iframe>
+                    </div>
+                    <br>
+                    <p class="small text-center">View the WLOS article <a href="https://wlos.com/news/local/leaders-discuss-ways-to-help-everyone-in-the-mountains-have-access-to-internet" target="_blank">here</a>.</p>-->
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe src="https://www.youtube.com/embed/rJpFtDtMe9o" frameborder="0" allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+					<p class="small text-center"><a href="https://youtu.be/rJpFtDtMe9o">UNCA Broadband Summit</a></p>
+    			</div>
+    		</div>
+		</div>
+	</section>
+    <br>
+	<section class="bg-light pb-2 pt-2">
+  		<div class="container">
+      			<div class="row">
+      				<div class="col-md-2">
+                    	<br>
+                    	<img class="rounded-circle img-fluid" src="img/advocacy/terri_wells.jpg">
+					</div>
+                	<div class="col-md-6 my-auto">
+                		<h3>Broadband Priority</h3>
+             	    	<h5>Terri Wells</h5>
+                	</div>
+				</div>
+      			<br>
+      			<div class="col-md-8 mx-auto">
+                    <div class="embed-responsive embed-responsive-16by9">
+                    	<iframe src="https://www.youtube.com/embed/pHbpqVKYU2I" frameborder="0" allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+				</div>
+		</div>
+	</section>
+    <br><br><br>
+    <section class="pb-2 pt-2">
+    	<div class="container">
+      		<div class="row">
+      			<div class="col-md-2">
+      				<img class="rounded-circle img-fluid" src="img/advocacy/roy_cooper.jpg">
+      			</div>
+      			<div class="col-md-6 my-auto">
+      				<h3>Building Infrastructure for the 21st Century</h3>
+      				<h5>Governor Cooper</h5>
+      			</div>
+      		</div>
+      		<br>
+      		<p>As our state and economy continue to grow, North Carolina’s families and businesses need roads, bridges, and technology infrastructure to keep thriving. Governor Cooper is working with state and local leaders to address our state’s infrastructure needs.</p>
+          	<p>Governor Cooper and his Department of Transportation successfully passed the Build NC Bond Act with support from Democrats and Republicans. This law will help expedite critical highway projects, improving the everyday lives of people all over the state and encouraging businesses to grow and locate in North Carolina.</p>
+          	<p>Governor Cooper also knows that access to broadband is a must for economic success in our rural communities and across the state. He proposed funding to improve internet access and service to households and businesses in underserved areas of North Carolina, including a grant program to help local governments partnering with private providers and utility cooperatives complete ‘last mile’ broadband projects.</p>
+          	<p class="text-right"><a href="https://governor.nc.gov/issues/infrastructure" target="_blank">Read More &gt;</a></p>
+      	</div>
+	</section>
+    <br><br><br>
+    <section class="bg-light pb-2 pt-2">
+    	<div class="container">
+      		<div class="row">
+      			<div class="col-md-2">
+      				<img class="rounded-circle img-fluid" src="img/advocacy/dean_arp.jpg">
+      			</div>
+      			<div class="col-md-6 my-auto">
+                	<h3>Republican Rep. Dean Arp comments on GREAT program</h3>
+                  	<h5>Dean Arp</h5>
+      			</div>
+      		</div>
+      		<br>
+            <p>Rep. Dean Arp (R-Union), a House Appropriations Committee co-chair who led the creation and implementation of the GREAT program, said H.B. 387 provides forward-looking investments to benefit North Carolina’s rural counties and increase economic connectivity statewide.</p>
+            <p>“Broadband is an essential utility for all North Carolinians in today’s global economy, and the GREAT program is successfully delivering on the General Assembly’s promise to encourage and assist providers in deploying internet connectivity at the highest possible speeds,” Rep. Arp said Tuesday.</p>
+            <p>North Carolina’s GREAT program uniquely incentivizes broadband companies to provide higher speeds to ensure that as much of the state as possible is well-positioned with broadband service for the future.</p>
+             <p class="text-right"><a href="https://nchouserepublicans.com/2019/10/08/cooper-veto-rural-broadband/">Read More &gt;</a></p>
+   		</div>
+	</section>
+    <br><br>
+	<section class="pb-2 pt-2">
+ 		<div class="container">
+  			<h2 class="text-center">Policy in WNC</h2>
+			<br>
+            <!--Inlude links to stories about policy in WNC // i.e., the portion from Gov. Cooper-->
+          	<div class="col-9 mx-auto">
+          		<h5>Governor Cooper Launches Initiative to Increase Broadband Connectivity</h5>
+          		<p class="lead">Executive Order No. 91 Continues North Carolina's Work to Implement High-Speed Internet Across the State</p>
+          		<a href="https://wncbroadband.org/blog/governor-cooper-launches/" class="text-right"><p>Read More &gt;</p></a>
+  			</div>
+  			<br><br><br><br>
+                <!--<h3>WNC Public Officials</h3>-->
+                <!--Link to public officials in WNC-->
+  		</div>
+	</section>
+
+	<div class="separate"></div>
+   
+    <!--Steps You Can Take-->
+	<section class="pt-5 pb-5 bg-light">
+		<div class="container" id="sec3">
+			<h1 class="text-center">Steps You Can Take</h1>
+			<br>
+			<div class="row">
+				<div class="col-md-3 col-5 my-auto mx-auto">
+					<img class="img-fluid" src="img/getinvolved.png"/>
+				</div>
+				<div class="col-12 col-md-7 my-auto">
+					<h2>Get Involved</h2>
+					<br>
+					<p class="lead">Learn how to get started and take action for local and state policy positions to improve broadband in your area. See how you can get involved <a href="getinvolved.html">here</a>.</p>
+				</div>
+			</div>
+<!--<br><br><br>
+<h3>The Five Steps to Better Broadband</h3>
+<br>
+
+<div class="row mx-auto">
+<div class="col-md-2 col-5 mx-auto">
+<img class="img-fluid" src="img/understandBroadband.png">
+</div>
+<div class="col-md-8 mx-auto">
+<h4>Step 1: Understand Broadband in Your Area</h4>
+<p class="lead">Understand broadband, high-speed internet and related issues.</p>
+</div>
+</div>
+
+<div class="row mx-auto">
+<div class="col-md-2 col-5 mx-auto">
+<img class="img-fluid" src="img/takesurvey.png">
+</div>
+<div class="col-md-8 mx-auto">
+<h4>Step 2: Take Our Survey</h4>
+<p class="lead">Our short survey is used to measure use of the internet, customer satisfaction and future preferences.</p>
+</div>
+</div>
+
+<div class="row mx-auto">
+<div class="col-md-2 col-5 mx-auto">
+<img class="img-fluid" src="img/speedtest.png">
+</div>
+<div class="col-md-8 mx-auto">
+<h4>Step 3: Run a Speed Test</h4>
+<p class="lead">This speed test takes a measurement of current upload and download internet speeds. You can take the test <a href="survey/speedtest.php"> here</a>.</p>
+</div>
+</div>
+
+<div class="row mx-auto">
+<div class="col-md-2 col-5 mx-auto">
+<img class="img-fluid" src="img/viewMap.png">
+</div>
+<div class="col-md-8 mx-auto">
+<h4>Step 4: Speed in Your Community</h4>
+<p class="lead">View our map of your community's upload and download speeds.</p>
+</div>
+</div>
+
+<div class="row mx-auto">
+<div class="col-md-2 col-5 mx-auto">
+<img class="img-fluid" src="img/getinvolved.png">
+</div>
+<div class="col-md-8 mx-auto">
+<h4>Step 5: Get Involved!</h4>
+<p class="lead">Learn how to get started and take action for local and state policy positions to improve broadband in your area. See how you can get involved <a href="getinvolved.html">here</a>.</p>
+</div>
+</div>-->
+<!--Just the Get Involved Image and Get Involved-->
+			<br>
+    		<h3 class="mb-4 text-center">Take Action!</h3>
+    		<ol>
+          		<li class="mb-3">Join with the WNC Broadband Project, the State Broadband Office or Connect USA to volunteer to join the effort. <a href="contact.php">Contact WNC Broadband Project members</a>.</li>
+          		<li class="mb-3">Form or join a local committee to analyze broadband speeds and availability.</li>
+                <li class="mb-3">Promote broadband friendly policies with decision-makers you are acquainted with or who represent you.</li>
+                <li class="mb-3">Urge governmental and community leaders to communicate directly with providers about the state of current and future service levels.  Ask these local leaders to identify underserved neighborhoods and areas, to bring them to the attention of both the community and their service providers.</li>
+                  <!--
+                  <li class="mb-3">Urge governments, homeowner associations and developers to practice “dig once” policies where fiber optic conduit is installed when developing land, building sidewalks, creating public pathways, or constructing highways.</li>
+                  <li class="mb-3">Seek local funding from foundations and individuals to reduce the digital divide. </li>
+                  -->
+    		</ol>
+            <!--Include link to Get Involved Page-->
+		</div>
+	</section>
+
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+
+</body>
+</html>
