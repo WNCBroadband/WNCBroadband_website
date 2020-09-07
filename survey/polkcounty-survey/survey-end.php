317309af7e3e31f87dc501fa4a66b427b6fe92e5
Elias Landsman <elandsma@unca.edu>
2020-09-06 20:00:26 -0400

    added Polk County survey

---
+++ survey/polkcounty-survey/survey-end.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>WNC Broadband Project | Survey</title>
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- JQuery -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.min.js"></script> 
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!-- Custom fonts for this template -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	<!--Site Stylesheet-->
+	<link href="https://wncbroadband.org/css/style.css" rel="stylesheet" type="text/css">
+	<!-- Bootstrap core JavaScript -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.slim.min.js"></script>
+	<script src="https://wncbroadband.org/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	<?php include("http://www.wncbroadband.org/includes/nav_main_core.php");?>
+  
+    <!-- Page Content -->
+ 	<section class="p-3"></section>
+	<section class="header">  
+  		<div class="custom-container">
+        	<h1 class="mt-3 mb-5 text-center">Thank you for taking our survey!</h1>
+        	<p class="lead">Thank you for your participation in this survey. <br>
+        	Learn more about how you can get involved and get further engaged in broadband issues <a href="https://wncbroadband.org/blog/advocacy/"><b>here</b></a>.<br><br>
+        	<a href="http://www.wncbroadband.org/blog">Return to WNC Broadband Project homepage</a></p>
+  		</div>
+	</section>
+	<div class="separate"></div>
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+</body>
+</html>
