5b57cfd0b54cdc61e8743304bc660bfd22e338dd
Elias Landsman <elandsma@unca.edu>
2020-09-27 20:11:27 -0400

    fixed a layout error

--- aboutproject.php
+++ aboutproject.php
@@ -39,7 +39,6 @@
   			</div>
   		</div>
 	</section>
-
 	<section class="p-5">
 		<div class="container">
 	  		<h2>About the WNC Broadband Project</h2>
@@ -58,16 +57,17 @@
 	<section class="p-5 bg-light">
   		<div class="container">
   			<h2>Affiliations</h2>
-  				<h4>WestNGN</h4>
-  					<p>West Next Generation Network (WestNGN) Broadband is a regional initiative focused on stimulating the deployment of high-speed broadband networks in Western North Carolina. WestNGN's goal is to encourage private sector providers to deliver ultra-fast bandwidth at affordable prices to ensure our region remains competitive and at the forefront of emerging next-generation applications essential to all sectors of the economy.</p>
-  					<p>This effort is a collaboration by six municipalities, four counties, and two universities with support from Land of Sky Regional Council. As of Sept. 2019, WestNGN has sponsored three leadership summits on broadband, recruited at least one new provider to the region, conducted a survey of 8,000 residents, developed tools to assist communities (such as this website) and worked closely with state and local officials to promote better broadband services. In addition, Land of Sky is working with local governmental units to map vertical assets, provide advice, and build governmental support for broadband improvement.</p>
-  					<h5 class="mt-3">WestNGN Contact Information</h5>
-					  <div class="row mb-5">
+  			<h4>WestNGN</h4>
+  				<p>West Next Generation Network (WestNGN) Broadband is a regional initiative focused on stimulating the deployment of high-speed broadband networks in Western North Carolina. WestNGN's goal is to encourage private sector providers to deliver ultra-fast bandwidth at affordable prices to ensure our region remains competitive and at the forefront of emerging next-generation applications essential to all sectors of the economy.</p>
+  				<p>This effort is a collaboration by six municipalities, four counties, and two universities with support from Land of Sky Regional Council. As of Sept. 2019, WestNGN has sponsored three leadership summits on broadband, recruited at least one new provider to the region, conducted a survey of 8,000 residents, developed tools to assist communities (such as this website) and worked closely with state and local officials to promote better broadband services. In addition, Land of Sky is working with local governmental units to map vertical assets, provide advice, and build governmental support for broadband improvement.</p>
+  				<h5 class="mt-3">WestNGN Contact Information</h5>
+				<div class="row mb-5">
 					  <!--
 					    <div class="col-sm-4">
 					      <p class="lead mb-0">William Sederburg</p>
 					      <p>Chair<br>
 					      <a href="mailto:wsederburg@gmail.com">wsederburg@gmail.com</a></p>
+					    
 					    </div>
 					    -->
 					    <div class="col-sm-4">
@@ -83,16 +83,16 @@
 					    </div>
 					    -->
 				</div>
-  			</div>
   		</div>
 	</section>
 	<section class="p-5">
-		<h2>Additional Information:</h2>
-	     <p>Land of Sky and WestNGN Broadband (Visit <a href="http://www.landofsky.org/westngnbroadband.html" target="_blank">landofsky.org/westngnbroadband</a>)</p>
-	     <p>Broadband 101 -- update on WestNGN April 2019 Summit (View <a href="files/LandofSky_April_2019.pdf" target="_blank">slideshow</a>).</p>
-	     <p>"Accelerating Opportunities' &mdash; a report on future broadband needs &mdash; Aug. 2017" (View <a href="files/AcceleratingOpportunities.pdf" target="_blank">PDF</a>)</p>
+		<div class=container>
+			<h2>Additional Information:</h2>
+    	     <p>Land of Sky and WestNGN Broadband (Visit <a href="http://www.landofsky.org/westngnbroadband.html" target="_blank">landofsky.org/westngnbroadband</a>)</p>
+    	     <p>Broadband 101 -- update on WestNGN April 2019 Summit (View <a href="files/LandofSky_April_2019.pdf" target="_blank">slideshow</a>).</p>
+    	     <p>"Accelerating Opportunities' &mdash; a report on future broadband needs &mdash; Aug. 2017" (View <a href="files/AcceleratingOpportunities.pdf" target="_blank">PDF</a>)</p>
+		</div>
 	</section>
-
 	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
 </body>
 </html>
