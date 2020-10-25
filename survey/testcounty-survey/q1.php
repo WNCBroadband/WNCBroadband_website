eb536bb55ae4ffbbf2a95ff3dc940bd24eddbf29
Elias Landsman <elandsma@unca.edu>
2020-10-25 17:53:06 -0400

    added test survey with questions that match state survey.

---
+++ survey/testcounty-survey/q1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>WNC Broadband Project | Survey</title>
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- JQuery -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.min.js"></script> 
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!-- Custom fonts for this template -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	<!--Site Stylesheet-->
+	<link href="https://wncbroadband.org/css/style.css" rel="stylesheet" type="text/css">
+	<!-- Bootstrap core JavaScript -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.slim.min.js"></script>
+	<script src="https://wncbroadband.org/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	<!-- Navigation -->
+	<?php include("http://www.wncbroadband.org/includes/nav_main_core.php");?>
+	<section class="header bg-broad pt-5">  
+  		<div class="container pt-5">
+  			<div class="row">
+  				<div class="col-lg-7">
+  					<h1>Macon County</h1>
+  				</div>
+  			</div>
+  		</div>
+	</section>	
+	<section class="p-3 bg-light"></section>
+	<section class="p-5 bg-light">
+		<div id="survey-bg" class="row">
+			<div class="col-lg-6 mx-auto">
+				<div class="form-group">
+			    	<form class="form">
+			      		<h3>Does your household currently subscribe to a non-cellular broadband internet service provider (cable, DSL, satellite)?</h3>
+			      		<br>
+			      		<a class="btn btn-primary btn-lg mt-auto mb-5" href="take_survey.php">Yes</a>
+			      		<br>
+			      		<a class="btn btn-primary btn-lg mt-auto mb-5" href="take_survey_no.php"> No </a>
+		        	</form>
+		      	</div>
+			</div>
+			<div class="my-auto showcase-text offset-lg-3 survey-bg col-lg-6">
+			</div>
+		</div>	
+	</section>
+	<div class="separate"></div>
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+</body>
+</html>
