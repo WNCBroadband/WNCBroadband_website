54e8becb707c5daddf2e366b516d87ea319e3b30
Elias Landsman <elandsma@unca.edu>
2021-01-17 21:09:25 -0500

    Beginning demo page for summit material presentation.

---
+++ demo.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>WNC Broadband Project | WNC Summit</title>
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- JQuery -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.min.js"></script> 
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!-- Custom fonts for this template -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	<!--Site Stylesheet-->
+	<link href="https://wncbroadband.org/css/style.css" rel="stylesheet" type="text/css">
+	<!-- Bootstrap core JavaScript -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.slim.min.js"></script>
+	<script src="https://wncbroadband.org/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+  <!-- Navigation -->
+  	<script>
+// 		var navid = "about";
+//  	makes "about" link active in nav
+	</script>
+	<?php include("http://www.wncbroadband.org/includes/nav_main_core.php");?>
+
+	<!-- Page Content -->
+	<section class="header bg-broad pt-5">  
+  		<div class="container pt-5">
+  			<div class="row">
+  				<div class="col-lg-7">
+  					<h1>WNC Broadband Summit Materials</h1>
+  				</div>
+  			</div>
+  		</div>
+	</section>
+	<section class="p-5">
+		<div class="container">
+	  		<h2>Lorem Ipsum</h2>
+	  		<p class="lead">Kindling the energy hidden in matter cosmos trillion Drake Equation not a sunrise but a galaxyrise galaxies. Hearts of the stars are creatures of the cosmos realm of the galaxies rings of Uranus cosmic ocean circumnavigated? Courage of our questions invent the universe invent the universe ship of the imagination descended from astronomers descended from astronomers. </p>
+	  		<p>Brain is the seed of intelligence preserve and cherish that pale blue dot brain is the seed of intelligence something incredible is waiting to be known bits of moving fluff invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
+	  		<a class="btn btn-primary mb-5" href="#">Go somewhere &gt;</a>
+	  		&nbsp;&nbsp;&nbsp;
+	  		<a class="btn btn-primary mb-5" href="#">Go somewhere else &gt;</a>
+	  		<!--<h4>WNC Broadband Project Links</h4>
+			 <ul>
+			    <li>
+			    </li>
+			  </ul>-->
+		</div>
+	</section>
+	<section class="p-5 bg-light">
+  		<div class="container">
+  			<h2>Another Section</h2>
+  			<h4>Smaller Header</h4>
+  			<p>Invent the universe billions upon billions circumnavigated the carbon in our apple pies quis nostrum exercitationem ullam corporis suscipit laboriosam tendrils of gossamer clouds? Preserve and cherish that pale blue dot citizens of distant epochs something incredible is waiting to be known citizens of distant epochs Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium radio telescope. Descended from astronomers the only home we've ever known take root and flourish at the edge of forever Ut enim ad minima veniam star stuff harvesting star light and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>  			
+  			<h5 class="mt-3">H5 header.</h5>
+  			<script>  
+  				$( function() {
+    				$( "#accordion" ).accordion();
+  				} );
+  			</script>  				
+  			<div id="accordion">
+  				<h3>I. Availability & Affordability of Broadband</h3>
+  				<div>first content panel</div>
+  				<h3>II. </h3>
+  				<div>second content panel</div>
+  				<h3>III. </h3>
+  				<div>Third content panel</div>
+  				<h3>IV. </h3>
+  				<div>Fourth content panel</div>
+  				
+  			</div>
+  		</div>
+  		
+	</section>
+	<section class="p-5">
+		<div class=container>
+			<h2>Additional Information:</h2>
+    	     <p>Land of Sky and WestNGN Broadband (Visit <a href="http://www.landofsky.org/westngnbroadband.html" target="_blank">landofsky.org/westngnbroadband</a>)</p>
+    	     <p>Broadband 101 -- update on WestNGN April 2019 Summit (View <a href="files/LandofSky_April_2019.pdf" target="_blank">slideshow</a>).</p>
+    	     <p>"Accelerating Opportunities' &mdash; a report on future broadband needs &mdash; Aug. 2017" (View <a href="files/AcceleratingOpportunities.pdf" target="_blank">PDF</a>)</p>
+		</div>
+	</section>
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+</body>
+</html>
