ba715dbdf25aec957ee0e138e303520596ef8278
Elias <eplandsman@gmail.com>
2022-04-10 13:34:24 -0400

    add student projects page

---
+++ students.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>WNC Broadband Project | UNCA MassComm Senior Projects</title>
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- JQuery -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.min.js"></script> 
+	<!-- Bootstrap core CSS -->
+	<link href="https://wncbroadband.org/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!-- Custom fonts for this template -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	<!--Site Stylesheet-->
+	<link href="https://wncbroadband.org/css/style.css" rel="stylesheet" type="text/css">
+	<!-- Bootstrap core JavaScript -->
+	<script src="https://wncbroadband.org/vendor/jquery/jquery.slim.min.js"></script>
+	<script src="https://wncbroadband.org/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	
+
+</head>
+<body>
+
+  <!-- Navigation -->
+  	<script>
+// 		var navid = "about";
+//  	makes "about" link active in nav
+	</script>
+	<?php include("http://www.wncbroadband.org/includes/nav_main_core.php");?>
+
+	<!-- Page Content -->
+	<section class="header bg-broad pt-5">  
+  		<div class="container pt-5">
+  			<div class="row">
+  				<div class="col-lg-9">
+  					<h1>UNCA Student Projects for WNC Broadband</h1>
+  				</div>
+  			</div>
+  		</div>
+	</section>
+	<section class="p-5">
+		<div class="container">
+	  		<h2>UNCA Mass Communications Student Projects</h2>
+	  		<p class="lead">Students create content for the WNC Broadband project </p>
+	  		<p>In Spring 2022, Dr. DiPalma led a senior seminar in the Mass Communication department at UNC Asheville. Throughout the semester, students created a variety of multimedia projects for the WNC Broadband group.</p>
+            
+            <!-- <a class="btn btn-primary mb-5" href="https://wncbroadband.org/blog/broadband-bulldogs/">Go somewhere &gt;</a>
+	  		&nbsp;&nbsp;&nbsp;
+	  		<a class="btn btn-primary mb-5" href="#">Go somewhere else &gt;</a> -->
+		</div>
+	</section>
+	<section class="p-5 bg-light">
+  		<div class="container">
+  			<h2>Project Details</h2>
+  			<h4>Topics</h4>
+  			<p>
+                The students engaged in topics such as:
+                <ul>
+                    <li>Broadband Policy
+                    <li>Digital Divide
+                    <li>Affect of the pandemic upon broadband
+                </ul>           
+            </p>  			
+  		</div>
+	</section>
+	<section class="p-5">
+		<div class=container>
+			<h2>Additional Information:</h2>
+    	     <p>Contact Dr. DiPalma, or another member of the WNC Broadband team, for more information</p>
+             <a class="btn btn-primary mb-5" href="https://wncbroadband.org/contact.php">Contact Us</a>
+		</div>
+	</section>
+	<?php include("http://www.wncbroadband.org/includes/footer.php");?>
+</body>
+</html>
